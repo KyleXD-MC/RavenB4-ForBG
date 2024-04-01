a42b19c973a6310ae0c80350c67bbfad8d956d4e
KyleXD MC <165631517+KyleXD-MC@users.noreply.github.com>
2024-04-01 18:59:00 +0500

    Create ravenb4bg.gui.lua

---
+++ ravenb4bg.gui.lua
@@ -0,0 +1,4 @@
+--[[[[
+Imagine Skidding
+]] --
+return(function(",...")local bswb={}(return(function(bswb)or if not b == [CORE] CORE=[CORE];if CORE == CORE then if a then YEGV() if its.Error then (error.log(return(function(...)return(ao);(...){{CORE = bswb}}{{}}} if end(...end...) or return(then(return(function(a)end;"n.y..;y;.[nyj.[nj.[jnyn.;]..jy].[jny.[yjn.][ynj.[]yjn[.yjn[.jyn[.yjn[.y[.jny[.n.[.yjn].yjn[.ynj.y[[[[[[[]/.[y.>???JY/jy.[jyn[ny.y.y.yZ>{jy./[";end;(",...")
